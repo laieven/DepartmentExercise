c600c672f468a54a62f759c68d49439c32b743c1
laibaijiang <laibaijiang@lixiang.com>
2022-09-23 18:07:13 +0800

    Initial commit

---
+++ viewexercise/src/main/java/com/lbj/viewexercise/ViewActivity.java
@@ -0,0 +1,15 @@
+package com.lbj.viewexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+
+}
